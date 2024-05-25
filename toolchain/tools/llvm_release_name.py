c07e91430f01996a258304c452226d0f8d950306
Jake Zimmerman <zimmerman.jake@gmail.com>
2024-05-24 17:25:05 -0700

    Allow building on 22.04

--- toolchain/tools/llvm_release_name.py
+++ toolchain/tools/llvm_release_name.py
@@ -110,7 +110,7 @@ def _linux(llvm_version):
         os_name = "linux-sles%s" % version
     elif distname == "ubuntu" and version.startswith("14.04"):
         os_name = "linux-gnu-ubuntu-14.04"
-    elif (distname == "ubuntu" or distname == "linuxmint" or distname == "pop") and version.startswith("20"):
+    elif (distname == "ubuntu" or distname == "linuxmint" or distname == "pop") and (version.startswith("20") or version.startswith("22")):
         if major_llvm_version < 11:
             # There is no binary packages specifically for 20.04, but those for 18.04 works on
             # 20.04
